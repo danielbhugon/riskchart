f1f0e97b6cbe6c77a01e70d0dbdb07769d7df8ee
Daniel <danielbhugon@gmail.com>
2015-12-07 00:43:12 +0000

    Market value function for trades

--- test/trade_test.js
+++ test/trade_test.js
@@ -64,4 +64,21 @@ describe('Trade', function() {
 		});
 	});
 	
+	describe('#marketValue(priceCurve)', function(){
+		
+		console.log(null*123213);
+		
+		it('should have market value equal to price times volume', function(){
+			assert.equal(v.trade2.marketValue(v.myCurve), v.trade2.totalVolume()*v.janBaseloadPrice.price);
+		});
+		
+		it('should have market value equal to price times volume', function(){
+			assert.equal(v.trade3.marketValue(v.myCurve), v.trade3.totalVolume()*v.aprBaseloadPrice.price);
+		});
+		
+		it('should have null market value if a matching market price is not found', function(){
+			assert.equal(v.trade1.marketValue(v.myCurve), null);
+		});
+	});
+	
 });
