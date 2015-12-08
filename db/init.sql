ec2775f8fb09b866062e7788475b3b408b958bb4
Daniel <danielbhugon@gmail.com>
2015-12-08 00:12:07 +0000

    Insert dummy trades into static data

--- db/init.sql
+++ db/init.sql
@@ -67,23 +67,44 @@ CREATE TABLE IF NOT EXISTS portfolios
 
 -- dummy data to get us started
 DELETE FROM products;
-INSERT INTO products (name, rate_label, time_unit, volume_label, price_unit)
+INSERT INTO products (id, name, rate_label, time_unit, volume_label, price_unit)
 VALUES
-('baseload', 'MW', 'h', 'MWh', '£'),
-('peak', 'MW', 'h', 'MWh', '£');
+(0, 'baseload', 'MW', 'h', 'MWh', '£'),
+(1, 'peak', 'MW', 'h', 'MWh', '£');
 
 DELETE FROM contracts;
-INSERT INTO contracts (name, start_date, end_date, time_amount)
+INSERT INTO contracts (id, name, start_date, end_date, time_amount)
 VALUES
 -- note that end dates are exclusive, i.e. treated as ending at 00:00 hours on the specified dates
-('Jan-16', '2016-01-04', '2016-02-01', 672),
-('Feb-16', '2016-02-01', '2016-02-29', 672),
-('Mar-16', '2016-02-29', '2016-04-04', 840),
-('Q1-16',  '2016-01-04', '2016-04-04', 2184);
+(0, 'Jan-16', '2016-01-04', '2016-02-01', 672),
+(1, 'Feb-16', '2016-02-01', '2016-02-29', 672),
+(2, 'Mar-16', '2016-02-29', '2016-04-04', 840),
+(3, 'Q1-16',  '2016-01-04', '2016-04-04', 2184);
 
+DELETE FROM users;
+INSERT INTO users (id, name)
+VALUES
+(0, 'Powerisk');
 
+DELETE FROM portfolios;
+INSERT INTO portfolios (user_id, name)
+VALUES
+(0, 'Portfolio A'),
+(0, 'Portfolio B'),
+(0, 'Portfolio C'),
+(0, 'Portfolio D');
 
-
+DELETE FROM trades;
+INSERT INTO trades (portfolio_id, product_id, contract_id, rate_amount, price, trade_date, info)
+VALUES
+(0,0,0,10,54.75,'2014-06-24','test'),
+(0,0,0,10,53.6,'2014-07-10','test'),
+(0,0,0,10,55.4,'2014-07-24','test'),
+(0,0,0,10,56.15,'2014-09-16','test'),
+(0,0,0,10,51.66,'2014-12-18','test'),
+(0,0,0,20,47.15,'2015-01-14','test'),
+(0,0,0,20,45.86,'2015-06-17','test'),
+(0,0,0,15,43.79,'2015-09-16','test');
 
 
 
